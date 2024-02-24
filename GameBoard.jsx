5f5c979e7652cdc085a4c6ca812193032cb49f6f
kjlee1203 <kjuni1@naver.com>
2024-02-25 05:24:14 +0900

    gameboard - mark X when clicked

--- GameBoard.jsx
+++ GameBoard.jsx
@@ -1,24 +1,42 @@
+import { useState } from "react";
+
 const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-  ];
-  
-  export default function GameBoard() {
-    return (
-      <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => (
-          <li key={rowIndex}>
-            <ol>
-              {row.map((playerSymbol, colIndex) => (
-                <li key={colIndex}>
-                  <button>{playerSymbol}</button>
-                </li>
-              ))}
-            </ol>
-          </li>
-        ))}
-      </ol>
-    );
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      // prevGameBoard[rowIndex][colIndex] = "X"; is  not recommended.
+      // make copy using '...'
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+
+      return updatedBoard;
+    });
   }
-  
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
