e730c9691f5fee07c292e5071ea21be62ad066f1
kjlee1203 <kjuni1@naver.com>
2024-02-25 04:38:48 +0900

    we can now edit the player names

--- Player.jsx
+++ Player.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   // whether we are editing the player or not
   const [isEditing, setIsEditing] = useState(false);
 
@@ -11,18 +12,25 @@ export default function Player({ name, symbol }) {
     setIsEditing((editing) => !editing);
   }
 
+  function handleChange(event) {
+    // handle typing in the input box
+    setPlayerName(event.target.value);
+  }
+
   // if state is false display player name, edit button
   // else display input box, save button
-  let playerName = <span className="player-name">{name}</span>;
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
