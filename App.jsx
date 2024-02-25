5d20fd518b358b9f44b6179df7211bedb96c0667
kjlee1203 <kjuni1@naver.com>
2024-02-25 12:07:31 +0900

    derive active player from the log

--- App.jsx
+++ App.jsx
@@ -4,26 +4,41 @@ import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 
+function deriveActivePlayer(gameTurns) {
+  // get active player from game turns:
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
+
 function App() {
   // to make a log. but the info is generated in GameBoard
   // need to lift the state up
   const [gameTurns, setGameTurns] = useState([]);
+  const activePlayer = deriveActivePlayer(gameTurns);
 
+  /* We now derive active player from gameTurns, we dont need this anymore:
   // to pass info of which player is active
   // to both Player and GameBoard
   // initial active player is 'X'
   const [activePlayer, setActivePlayer] = useState("X");
+  */
 
   function handleSelectSquare(rowIndex, colIndex) {
     // if active player is x, change it into o, or vice versa
-    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
+      /*
       // current player is X in the first turn.
       let currentPlayer = "X";
       // latest turn is stored in [0] of the array.
       if (prevTurns.length > 0 && prevTurns[0].player === "X") {
         currentPlayer = "O";
       }
+      */
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: activePlayer },
